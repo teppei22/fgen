460676665e9219169e10a6aff62810177d651d6c
teppei.fujii <teppeif22@gmail.com>
2022-03-30 15:58:23 +0900

    Add: auto code generator for jennifer

---
+++ gen/jen/model_generator.go
@@ -0,0 +1,64 @@
+package gen
+
+import (
+	"fmt"
+	"strings"
+
+	jen "github.com/dave/jennifer/jen"
+)
+
+type ModelGenerator interface {
+	generate()
+}
+
+type Model struct {
+	Generator ModelGenerator
+}
+
+func NewModel() *Model {
+	return &Model{}
+}
+
+func (g *Model) generate() {
+
+	f := jen.NewFile("model")
+
+	f.Type().Id("Task").Struct(
+		jen.Id("ID").Id("int").Tag(map[string]string{"json": CamelToSnake("ID")}),
+		jen.Id("Title").Id("string").Tag(map[string]string{"json": CamelToSnake("Title")}),
+		jen.Id("Description").Id("string").Tag(map[string]string{"json": CamelToSnake("Description")}),
+		jen.Id("CreatedAt").Id("time.Time").Tag(map[string]string{"json": CamelToSnake("CreatedAt")}),
+		jen.Id("UpdatedAt").Id("time.Time").Tag(map[string]string{"json": CamelToSnake("UpdatedAt")}),
+	)
+
+	if err := f.Save(`./sample_model.go`); err != nil {
+		fmt.Printf("Error: %#v\n", fmt.Errorf("failed to save file"))
+	}
+
+	// fmt.Printf("%#v", f)
+}
+
+func CamelToSnake(s string) string {
+	if s == "" {
+		return s
+	}
+
+	delimiter := "_"
+	len := len(s)
+	var snake string
+	for i, curr := range s {
+		if i > 0 && i+1 < len {
+			if curr >= 'A' && curr <= 'Z' {
+				next := s[i+1]
+				prev := s[i-1]
+				if (next >= 'a' && next <= 'z') || (prev >= 'a' && prev <= 'z') {
+					snake += delimiter
+				}
+			}
+		}
+		snake += string(curr)
+	}
+
+	snake = strings.ToLower(snake)
+	return snake
+}
---
+++ gen/jen/persistence_generator.go
@@ -0,0 +1,62 @@
+package gen
+
+import (
+	"log"
+
+	jen "github.com/dave/jennifer/jen"
+)
+
+type PersistenceGenerator interface {
+	Generate()
+}
+
+type Persistence struct {
+	Generator PersistenceGenerator
+}
+
+func NewPersistence() *Persistence {
+	return &Persistenceπ{}
+}
+
+func (g *Persistence) Generate() {
+
+	f := jen.NewFile("persistence")
+
+	f.Type().Id("taskPersistence").Struct(
+		jen.Id("Conn").Id("*gorm.DB"),
+	)
+
+	// if err := f.Save(`./sample_model.go`); err != nil {
+	// 	fmt.Printf("Error: %#v\n", fmt.Errorf("failed to save file"))
+	// }
+
+	f.Func().Id("NewTask").Params(jen.Id("conn").Id("*gorm.DB")).Params(jen.Id("repository").Op(".").Id("TaskRepository")).Block(
+		jen.Return(jen.Id("&taskPersistence").Block(jen.Id("Conn:").Id("conn").Op(","))),
+	)
+
+	f.Func().Params(jen.Id("p").Id("*taskPersistence")).Id("FindByID").Params(jen.Id("id").Id("int")).Params(jen.Id("*model.Task"), jen.Id("error")).Block(
+
+		jen.Id("task").Op(":=").Op("&").Id("model").Id("Task").Block(jen.Id("ID").Op(":").Id("id"))
+	)
+
+	f.Line()
+	f.Func().Params(jen.Id("p").Id("*taskPersistence")).Id("Create").Params(jen.Id("*model.Task")).Params(jen.Id("*model.Task"), jen.Id("error")).Block(
+	// jen.Return(jen.Nil),
+	)
+
+	f.Line()
+	f.Func().Params(jen.Id("p").Id("*taskPersistence")).Id("Update").Params(jen.Id("*model.Task")).Params(jen.Id("*model.Task"), jen.Id("error")).Block(
+	// jen.Return(jen.Nil),
+	)
+
+	f.Line()
+	f.Func().Params(jen.Id("p").Id("*taskPersistence")).Id("Delete").Params(jen.Id("*model.Task")).Params(jen.Id("*model.Task"), jen.Id("error")).Block(
+	// jen.Return(jen.Nil),
+	)
+
+	if err := f.Save(`./output/sample_persistence.go`); err != nil {
+		log.Fatal(err)
+	}
+
+	// fmt.Printf("%#v", f)
+}
